376efde6a31ac90eb610d87b814cf39f5f1dba66
evtepo@gmail.com <lemana144>
2022-11-22 17:25:54 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         Book theLordOfTheRing = new Book("The Lord Of The Ring", 1955, new Author("John ", "Tolkien"));
         System.out.println(harryPotter.info());
         System.out.println(theLordOfTheRing.info());
-        System.out.println(theLordOfTheRing.getPublicateYear());
+        System.out.println(harryPotter.getPublicateYear());
+        harryPotter.setPublicateYear(2222);
+        System.out.println(harryPotter.getPublicateYear());
     }
 }
