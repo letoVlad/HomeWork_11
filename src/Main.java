c75f6eac80f80e00598e8a940d3514f61819872c
evtepo@gmail.com <lemana144>
2022-11-22 17:26:24 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,5 @@ public class Main {
         System.out.println(theLordOfTheRing.info());
         System.out.println(harryPotter.getPublicateYear());
         harryPotter.setPublicateYear(2222);
-        System.out.println(harryPotter.getPublicateYear());
     }
 }
